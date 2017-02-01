9788d6e5aa6077901fd098fb9b222fbd5524eed2
wteoh <norphos@gmail.com>
2017-02-02 04:58:33 +0800

    asd

--- js/itinerary.js
+++ js/itinerary.js
@@ -2,8 +2,11 @@
 
 //retrieve all itinerary
 $(document).on('pagebeforeshow', '#main', function() {
+  //check if itineray localstorage is undefined
+
+  var itinerary_json = localStorage.itinerary;
   //try to retrieve itinerary JSON, if JSON is null, display No itinerary created
-  var itinerary = JSON.parse(localStorage.itinerary);
+  var itinerary = JSON.parse(itinerary_json);
   if(itinerary === null)
   {
     $('#itinerary-ul').append("<li>No Trip Created</li>");
