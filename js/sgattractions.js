74499fb6ced87ee4063044f7563423264df2e85c
wteoh <norphos@gmail.com>
2017-02-01 13:09:33 +0800

    asd

--- js/sgattractions.js
+++ js/sgattractions.js
@@ -74,7 +74,7 @@ $(document).on('pagebeforeshow', '#attraction-details', function() {
     });
 });
 
-$(document).on('pagecreate', '#map', function() {
+$(document).on('pagebeforeshow', '#map', function() {
     var myLatlng = new google.maps.LatLng(51.520838, -0.140261);
     var myOptions = {
         zoom: 15,
