66f592cfa8084209abc86dd156ff3448367ccd35
wteoh <norphos@gmail.com>
2017-02-02 04:21:50 +0800

    asd

---
+++ js/itinerary.js
@@ -0,0 +1,19 @@
+//create new itinerary
+
+//retrieve all itinerary
+$(document).on('pagebeforeshow', '#main', function() {
+  //try to retrieve itinerary JSON, if JSON is null, display No itinerary created
+  var itinerary = JSON.parse($.getJSON(localStorage.itinerary));
+  if(itinerary === null)
+  {
+    $('#itinerary-ul').append("<li>No Trip Created</li>");
+  }
+
+});
+
+
+//view itinerary details
+
+//remove activity from itinerary
+
+//add activity to itinerary
