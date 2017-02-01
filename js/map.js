a497689482542754cf850c99d230728bb08d2a14
wteoh <norphos@gmail.com>
2017-02-01 12:11:11 +0800

    asdasd

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-function initialize() {
+$(document).on('pageinit', function() {
     var myLatlng = new google.maps.LatLng(51.520838, -0.140261);
     var myOptions = {
         zoom: 15,
@@ -7,4 +7,4 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map( document.getElementById( "map_canvas" ), myOptions );
-}
+});
