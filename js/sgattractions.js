7b289d4f9ae605938dd62470d68e4f527a37b33b
wteoh <norphos@gmail.com>
2017-02-02 23:11:14 +0800

    asd

--- js/sgattractions.js
+++ js/sgattractions.js
@@ -52,7 +52,7 @@ $(document).on('pagebeforeshow', '#attraction-details', function() {
 });
 
 
-$('#attraction-list').on('click', 'li a', function() {
+$('#attraction-list').on('click', 'a', function() {
     // if (typeof(Storage) !== "undefined") {
     //     //localStorage.attraction_id = $(this).attr("id");
     //     localStorage.attraction_id = $(this).attr("id");
