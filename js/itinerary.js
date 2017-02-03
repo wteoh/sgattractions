1757181713df0332e72146f46cfb88488f133bd5
wteoh <norphos@gmail.com>
2017-02-04 00:59:18 +0800

    sad

--- js/itinerary.js
+++ js/itinerary.js
@@ -35,6 +35,7 @@ $(document).on('click', '#btnCreate', function() {
             });
             localStorage.itinerary = JSON.stringify(trip);
             location.reload(true);
+
         }
     }
 });
@@ -59,7 +60,7 @@ $(document).on('pagebeforeshow', '#main', function() {
     } else {
         //otherwise, print all itinerary created
         $.each(itinerary, function(key, val) {
-            $('#itinerary-ul').append("<li class='ui-li-static ui-body-a'>" + val.name + "</li>");
+            $('#itinerary-ul').append("<li class='ui-li-static ui-body-a'><a href='#'>" + val.name + "</a></li>");
         });
     }
 
