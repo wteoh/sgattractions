fcfa916502225ef935895674b198c05df14fd64f
wteoh <norphos@gmail.com>
2017-02-05 17:08:00 +0800

    asd

--- js/itinerary.js
+++ js/itinerary.js
@@ -39,25 +39,7 @@ $(document).on('click', '#btnCreate', function() {
         }
     }
 
-    var toast = function(msg) {
-        $("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all'><h3>" + msg + "</h3></div>")
-            .css({
-                display: "block",
-                opacity: 0.90,
-                position: "fixed",
-                padding: "7px",
-                "text-align": "center",
-                width: "270px",
-                left: ($(window).width() - 284) / 2,
-                top: $(window).height() / 2
-            })
-            .appendTo($.mobile.pageContainer).delay(1500)
-            .fadeOut(400, function() {
-                $(this).remove();
-            });
-    }
-
-    //location.reload(true);
+    location.reload(true);
 
 });
 
@@ -114,7 +96,7 @@ function displayAttractions(attr) {
 $(document).on('click', '#attraction-list a', function() {
     localStorage.attraction_id = $(this).attr("id");
     //$.mobile.changePage("index.html#attraction-details");
-    window.location.href = "index.html#attraction-details";
+    window.location.href="index.html#attraction-details";
 
 });
 
